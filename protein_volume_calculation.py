9d612b355ff6f555db5fd0f602f9256551e72495
Lucelenie <lucelenie.rodriguez@choderalab.org>
2015-08-12 13:46:44 -0400

    Added program to calculate how muchprotein volume we need for a fluorescence assay.

---
+++ protein_volume_calculation.py
@@ -0,0 +1,14 @@
+# This program prompts user for concentration, molecular_weight and desired molarity
+# and outputs the amount of volume needed for the desired molarity.
+
+# Get molecular_weight and extinction coefficient from Peanut.
+# Src: MW 32689.6 Da, epsilon 52370 M^-1 cm^-1
+
+# Get concentration from the Denovix in mg/mL.
+
+concentration = input("What is the concentration of the protein in mg/mL? ")
+molecular_weight = input("What is the molecular weight of the protein? ")
+molarity = input("At what concentration you need the protein (molarity)? ")
+volume = round(((concentration / molecular_weight) / molarity)* 1000000, 1)
+print "You will need %r uL of your protein for it to be at %r uM." % (
+    volume, molarity)
